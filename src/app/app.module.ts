13473849ddfbc11dbbe475c4ab80b23fe9dda12a
AJIex1s <alexpeclug@mail.ru>
2017-09-16 23:04:09 +0300

    add simple styling for auth-form.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AuthFormComponent } from './auth-form/auth-form.component';
-import { MdInput, MdFormField } from '@angular/material';
+import { MdInputModule, MdFormFieldModule, MdButtonModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     BrowserModule,
-    MdInput
+    BrowserAnimationsModule,
+    MdFormFieldModule,
+    MdInputModule,
+    MdButtonModule
   ],
   declarations: [
     AppComponent,
